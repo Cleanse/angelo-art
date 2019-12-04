ab3647c08c8f5e30138bc65c3c74df7d00bad9f7
Paul E. Lovato <plovato@gmail.com>
2019-12-04 11:08:21 -0700

    Connect backend, database.

--- plugins/cleanse/kingofart/Plugin.php
+++ plugins/cleanse/kingofart/Plugin.php
@@ -2,6 +2,8 @@
 
 namespace Cleanse\KingOfArt;
 
+use Backend;
+use Controller;
 use System\Classes\PluginBase;
 
 class Plugin extends PluginBase
@@ -20,6 +22,34 @@ class Plugin extends PluginBase
     {
         return [
             'Cleanse\KingOfArt\Components\Featured' => 'cleanseKingFeatured'
+        ];
+    }
+
+    public function registerNavigation()
+    {
+        return [
+            'angelo' => [
+                'label' => 'King of Art',
+                'url' => Backend::url('cleanse/kingofart/creations'),
+                'icon' => 'icon-pencil',
+                'iconSvg' => 'plugins/cleanse/kingofart/assets/images/angelo.svg',
+                'permissions' => ['cleanse.kingofart.*'],
+                'order' => 33,
+                'sideMenu' => [
+                    'creations' => [
+                        'label' => 'Art List',
+                        'icon' => 'icon-copy',
+                        'url' => Backend::url('cleanse/kingofart/creations'),
+                        'permissions' => ['cleanse.kingofart.manage_art']
+                    ],
+                    'new_art' => [
+                        'label' => 'New Art',
+                        'icon' => 'icon-plus',
+                        'url' => Backend::url('cleanse/kingofart/creations/create'),
+                        'permissions' => ['cleanse.kingofart.manage_art']
+                    ]
+                ]
+            ]
         ];
     }
 }
