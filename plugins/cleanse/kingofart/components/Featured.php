720ba72b9710a6436c9c398a92dfb3610db2d7b3
Paul E. Lovato <plovato@gmail.com>
2019-12-04 11:25:35 -0700

    Featured component.

--- plugins/cleanse/kingofart/components/Featured.php
+++ plugins/cleanse/kingofart/components/Featured.php
@@ -4,13 +4,28 @@ namespace Cleanse\KingOfArt\Components;
 
 use Cms\Classes\ComponentBase;
 
+use Cleanse\KingOfArt\Models\Creation;
+
 class Featured extends ComponentBase
 {
+    private $featured;
+
     public function componentDetails()
     {
         return [
             'name' => 'Featured Art',
             'description' => 'Displays a collection of featured images.'
         ];
+    }
+
+    public function onRun()
+    {
+        $this->page['default_image'] = '/plugins/cleanse/kingofart/assets/images/angelo.svg';
+        $this->featured = $this->page['featured'] = $this->loadCreations();
+    }
+
+    protected function loadCreations()
+    {
+        return Creation::where('is_featured', 1)->orderBy('created_at', 'desc')->get();
     }
 }
