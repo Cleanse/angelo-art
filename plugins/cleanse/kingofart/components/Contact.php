037e3785645493100cfe1454bd621809d8a78716
Paul E. Lovato <plovato@gmail.com>
2019-12-20 13:56:23 -0700

    Backend logo for menu.

--- plugins/cleanse/kingofart/components/Contact.php
+++ plugins/cleanse/kingofart/components/Contact.php
@@ -6,13 +6,22 @@ use Cms\Classes\ComponentBase;
 
 class Contact extends ComponentBase
 {
-    private $contact;
-
     public function componentDetails()
     {
         return [
             'name' => 'Contact the Artist',
             'description' => 'Displays a contact form for the visitor.'
         ];
+    }
+
+    public function onContact()
+    {
+        $form = false;
+
+        if (!$form) {
+            return 'Please fill out all fields.';
+        }
+
+        return 't e x t';
     }
 }
